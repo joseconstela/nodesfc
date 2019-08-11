4d16811e8328f69d1b9bd5a5d910588cad703c6f
Jose Constela <jose@joseconstela.com>
2019-08-11 17:46:07 +0200

    Simplify example

--- example/run_from_nodejs.js
+++ example/run_from_nodejs.js
@@ -1,30 +1,3 @@
-let os = require('os')
-let path = require('path')
-
-let fs = require('fs')
-let nodesfc = require('../')
-
-let randomString = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-let file = {
-  name: `${os.tmpdir()}${path.sep}${randomString()}`
-}
-
-let inputCode = `
-/**
- * @dependency lodash latest
- */
-require('lodash').map([1,2,3,4], n => console.log(n))
-
-/**
- * @dependency faker latest
- */
-let fake = require('faker').fake('{{name.lastName}}, {{name.firstName}}')
-console.log(fake)`
-
-fs.writeFileSync(file.name, inputCode)
-
-nodesfc
-  .init({file: file.name})
-  .then(result => {
-    console.log({result})
-  })
+require('../')
+  .init({file: 'file.js'})
+  .then(result => console.log(JSON.stringify(result, ' ', 2)))
